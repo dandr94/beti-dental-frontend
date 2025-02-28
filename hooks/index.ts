6fabfd03f41054fb26cf0e15f1c5c65598daa03d
dandr94 <d.andreev1994@gmail.com>
2025-02-28 09:56:47 +0200

    update: update hooks export

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,6 +1,9 @@
-export { default as useRegister } from "./useRegister";
-export { default as useLogin } from "./useLogin";
 export { default as useActivation } from "./useActivation";
+export { default as useBookingModal } from "./useBookingModal";
+export { default as useCalendarData } from "./useCalendarData";
+export { default as useCalendarLogic } from "./useCalendarLogic";
+export { default as useLogin } from "./useLogin";
+export { default as useRegister } from "./useRegister";
 export { default as useResetPassword } from "./useResetPassword";
 export { default as useResetPasswordConfirm } from "./useResetPasswordConfirm";
 export { default as useUserDashboard } from "./useUserDashboard";
