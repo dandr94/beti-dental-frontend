64b8354799fa8a6689a21059e09f76e9a2b95763
dandr94 <d.andreev1994@gmail.com>
2025-06-25 23:31:20 +0300

    chore: add index file with imports

---
+++ hooks/admin/index.ts
@@ -0,0 +1,5 @@
+export { default as useAppointments } from "./useAppointments";
+export { default as useCreateUser } from "./useCreateUser";
+export { default as useDeleteUser } from "./useDeleteUser";
+export { default as useEditUser } from "./useEditUser";
+export { default as useScheduleLogic } from "./useScheduleLogic";
