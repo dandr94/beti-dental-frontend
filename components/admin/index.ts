a15df2ffb76c048f196f2345198043d6ccea9f63
dandr94 <d.andreev1994@gmail.com>
2025-06-25 23:20:44 +0300

    chore: add index file with imports

---
+++ components/admin/index.ts
@@ -0,0 +1,6 @@
+export { default as Sidebar } from "./Sidebar";
+export { default as Navbar } from "./Navbar";
+export { default as CreateUserModal } from "./CreateUserModal";
+export { default as EditUserModal } from "./EditUserModal";
+export { default as DeleteUserDialog } from "./DeleteUserModal";
+export { default as StatCard } from "./StatCard";
